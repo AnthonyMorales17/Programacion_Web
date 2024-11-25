9e27a4c5026726b6d740866c5bd6f2bf88841628
Anthony <103002304+AnthonyMorales17@users.noreply.github.com>
2024-11-25 00:59:44 -0500

    Deber_1

--- PrimerParcial/Deber_1/js/main.js
+++
@@ -1,9 +0,0 @@
-const title = document.querySelector('.card-content h2');
-    
-title.addEventListener('mouseover', () => {
-    document.body.style.backgroundColor = 'black';
-});
-
-title.addEventListener('mouseout', () => {
-    document.body.style.backgroundColor = '#fff';
-});
